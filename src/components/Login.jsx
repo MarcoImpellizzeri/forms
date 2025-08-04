ecf44159fe4787ca8f6a0e6a5a93f5107078bd17
Marco Impellizzeri <impellizzeri.marco18@gmail.com>
2025-08-04 10:53:00 +0200

    logica per salvare i dati inseriti nei campi di input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,47 @@
+import { useState } from 'react'
+
 export default function Login() {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    console.log(form)
+  }
+
+  function handleFormChange(identifier, value) {
+    setForm(prevState => ({
+      ...prevState,
+      [identifier]: value
+    }))
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => handleFormChange('email', e.target.value)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleFormChange('password', e.target.value)}
+          />
         </div>
       </div>
 
